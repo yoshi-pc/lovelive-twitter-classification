8f978dcd4c772fb61a41e495bde2ff1e81aeba89
yoshipc <yoooo@yoshipc.net>
2022-08-28 14:34:19 +0900

    最低限の表示機能をつけたもの

---
+++ make_html.py
@@ -0,0 +1,38 @@
+import joblib
+
+tweet_contents = joblib.load("./tweet_contents.jblb")
+
+
+page_data = {}
+content_dict = dict(
+    mus_content = "mus",
+    aqours_content = "aqours",
+    nijigaku_content = "nijigaku",
+    liella_content = "liella",
+    others_content = "others"
+)
+
+for cont, group in content_dict.items():
+    temp_text = []
+    for single_tweet in tweet_contents[group]:
+        img_class = "p-2 col-md-3"
+        img_link = "".join(["<img src=\"" + item + f"\" class=\"{img_class}\" />" for item in single_tweet["images"]])
+        text_origin = single_tweet["text"].replace("\n", "<br />")
+        temp_text.append(
+            f"""
+            <span class=\"mt-2\"><a href=\"https://twitter.com/LoveLive_staff/status/{single_tweet['id']}/\">original tweet</a></span><br />
+            <p class=\"pt-1\">{text_origin}</p>
+            {img_link}
+            """
+        )
+    page_data[cont] = temp_text
+
+with open("./server/index_template.html", "r") as f:
+    html = f.read()
+
+for cont, group in content_dict.items():
+    html = html.replace("{% " + cont + " %}", "<hr />".join(page_data[cont]))
+
+with open("./server/index.html", "w") as f:
+    f.write(html)
+pass
---
+++ retrieve.py
@@ -0,0 +1,64 @@
+import tweepy
+import re
+import unicodedata
+import joblib
+from pytz import timezone
+import os
+
+# set the token in your environment variable.
+BAERER = os.environ["twitter_baerer"]
+
+auth = tweepy.OAuth2BearerHandler(BAERER)
+api = tweepy.API(auth, wait_on_rate_limit = True)
+tweets = api.user_timeline(screen_name = "LoveLive_staff", count = 30, trim_user = True, tweet_mode = "extended", include_rts = False)
+
+tweet_ids = dict(
+    mus = [],
+    aqours = [],
+    nijigaku = [],
+    liella = [],
+    others = []
+)
+
+tweet_contents = dict(
+    mus = [],
+    aqours = [],
+    nijigaku = [],
+    liella = [],
+    others = []
+)
+
+search_pat = dict(
+    mus = r"(μ\'s|音ノ木坂)",
+    aqours = r"(Aqours|サンシャイン|浦の星|浦女)",
+    nijigaku = r"(ニジガク|虹ヶ咲|同好会)",
+    liella = r"(Liella|結ヶ丘|結女|スーパースター)"
+)
+
+pass
+
+for item in tweets:
+    text = unicodedata.normalize("NFKC", item.full_text)
+    hit = 0
+    for k, v in search_pat.items():
+        try:
+            img_urls = [single["media_url_https"] for single in item.extended_entities["media"]]
+        except AttributeError as e:
+            img_url = []
+        if re.search(v, text) is not None:
+            tweet_contents[k].append(dict(
+                id = item.id_str,
+                text = item.full_text,
+                images = img_urls,
+                time = item.created_at.astimezone(timezone('Asia/Tokyo')).strftime(r"%Y/%m/%d %H:%M:%S")
+            ))
+            hit += 1
+    if hit == 0:
+        tweet_contents["others"].append(dict(
+            id = item.id_str,
+            text = item.full_text,
+            images = img_urls,
+            time = item.created_at.astimezone(timezone('Asia/Tokyo')).strftime(r"%Y/%m/%d %H:%M:%S")
+        ))
+
+joblib.dump(tweet_contents, "./tweet_contents.jblb", compress = 3)
